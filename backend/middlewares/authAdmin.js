3cb1929eeabe9067b2ffe80f483a9ee9282c7975
Arka Sarkar <arkaskr24@gmail.com>
2026-05-29 22:13:13 +0530

    bypass fixed

--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -7,7 +7,7 @@ const authAdmin = async (req, res, next) => {
             return res.json({ success: false, message: "Not Authorized. Login Again" });
         }
         const token_decode = jwt.verify(token, process.env.JWT_SECRET);
-        if (token_decode.id !== "admin@movish.com" + "admin123") {
+        if (token_decode.id !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
             return res.json({ success: false, message: "Not Authorized. Login Again" });
         }
         next();
